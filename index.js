1a23e174c6d4a8385091a4206844f519a3e9ded6
Rajat Sharma <rajjusharmarajat@gmail.com>
2024-01-17 16:22:31 +0530

    panel add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const editor = grapesjs.init({
     container: '#gjs',
     fromElement: true,
-    height:'850px',
+    height:'600px',
     width: 'auto',
     storageManager: false,
     panels: {defaults:[]},
@@ -61,3 +61,39 @@ editor.blockManager.add('heading',{
         ]
     }
     })
+
+    editor.Panels.addPanel({
+        id: 'panel-top',
+        el: '.panel__top',
+      });
+      editor.Panels.addPanel({
+        id: 'basic-actions',
+        el: '.panel__basic-actions',
+        buttons: [
+          {
+            id: 'visibility',
+            active: true, // active by default
+            className: 'btn-toggle-borders',
+            label: '<u>B</u>',
+            command: 'sw-visibility', // Built-in command
+          }, {
+            id: 'export',
+            className: 'btn-open-export',
+            label: 'Exp',
+            command: 'export-template',
+            context: 'export-template', // For grouping context of buttons from the same panel
+          }, {
+            id: 'show-json',
+            className: 'btn-show-json',
+            label: 'JSON',
+            context: 'show-json',
+            command(editor) {
+              editor.Modal.setTitle('Components JSON')
+                .setContent(`<textarea style="width:100%; height: 250px;">
+                  ${JSON.stringify(editor.getComponents())}
+                </textarea>`)
+                .open();
+            },
+          }
+        ],
+      });
