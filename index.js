55daad0db4028ecfa7bd5fa675380714d3a8f96a
Rajat Sharma <rajjusharmarajat@gmail.com>
2024-01-18 11:14:50 +0530

    bootstrap added

--- index.js
+++ index.js
@@ -40,28 +40,6 @@ const editor = grapesjs.init({
       },
 })
 
-
-editor.blockManager.add('heading',{
-     label: 'heading',
-     category: 'heading',
-     content: {
-        tagName: 'div',
-        draggable: false,
-        attributes: { 'some-attribute': 'some-value' },
-        components: [
-          {
-            tagName: 'span',
-            content: '<b>Some static content</b>',
-          }, {
-            tagName: 'div',
-            // use `content` for static strings, `components` string will be parsed
-            // and transformed in Components
-            components: '<span>HTML at some point</span>',
-          }
-        ]
-    }
-    })
-
     editor.Panels.addPanel({
         id: 'panel-top',
         el: '.panel__top',
