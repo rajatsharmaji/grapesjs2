eeac1f2d5ea04d185c270c20d9eb91e537b38211
Rajat Sharma <rajjusharmarajat@gmail.com>
2024-01-18 13:50:39 +0530

    style added to text

--- index.js
+++ index.js
@@ -1,201 +1,227 @@
 const editor = grapesjs.init({
-    container: '#gjs',
-    fromElement: true,
-    attributes: { 'some-attribute': 'some-value' },
-    height:'600px',
-    width: 'auto',
-    storageManager: false,
-    panels: {defaults:[]},
+  container: "#gjs",
+  fromElement: true,
+  attributes: { "some-attribute": "some-value" },
+  height: "600px",
+  width: "auto",
+  storageManager: false,
+  panels: { defaults: [] },
 
-    blockManager: {
-        appendTo: '#blocks',
-        blocks: [
-          {
-            id: 'section',
-            label: `<b>section</b>`,
-            content: `<section>
-               <h1>Heading</h1>
-            </section>`
-          },    
-          {
-            id: 'text',
-            label: 'text',
-            content: `<section>
-            <p>paragraph</p>
-            </section>`
-          },
-          {
-            id: 'images',
-            label: 'images',
-            select: true,
-            content: {type : 'image'}
-          },
-          {
-            id: 'videos',
-            label: 'videos',
-            select: true,
-            content: {type : 'video'}
-          },
-          {
-            id: 'map',
-            label: 'map',
-            select: true,
-            content: {type : 'map'}
-          },
-          {
-            id: 'button',
-            label: 'button',
-            select: true,
-            content: `
+  blockManager: {
+    appendTo: "#blocks",
+    blocks: [
+      {
+        id: "heading",
+        label: `<b>heading</b>`,
+        content: `<section>
+               <h1 style="text-align: center; color:saddlebrown;">Heading</h1>
+            </section>`,
+      },
+      {
+        id: "text",
+        label: "text",
+        content: `<section>
+            <p style="">paragraph</p>
+            </section>`,
+      },
+      {
+        id: "images",
+        label: "images",
+        select: true,
+        content: { type: "image" },
+      },
+      {
+        id: "videos",
+        label: "videos",
+        select: true,
+        content: { type: "video" },
+      },
+      {
+        id: "map",
+        label: "map",
+        select: true,
+        content: { type: "map" },
+      },
+      {
+        id: "button",
+        label: "button",
+        select: true,
+        content: `
             <button type="button" class="btn">Button</button>
-            `
-          },
-        ]
+            `,
       },
-
-      layerManager: {
-        appendTo: '.layers-container'
+      {
+        id: "audio",
+        label: "audio",
+        select: true,
+        content: `
+            <audio controls>
+               <source src="" type="audio/ogg">
+               <source src="" type="audio/mpeg">
+               Your browser does not support the audio tag.
+            </audio>
+            `,
       },
+    ],
+  },
 
+  layerManager: {
+    appendTo: ".layers-container",
+  },
 
-      panels: {
-        defaults: [{
-          id: 'layers',
-          el: '.panel__right',
-          // Make the panel resizable
-          resizable: {
-            maxDim: 350,
-            minDim: 200,
-            tc: 0, // Top handler
-            cl: 1, // Left handler
-            cr: 0, // Right handler
-            bc: 0, // Bottom handler
-            keyWidth: 'flex-basis',
-          },
-          id: 'panel__switcher',
-          element: '.panel__switcher',
-          buttons: [
-            {
-              id: 'show-layers',
-              active: true,
-              label: 'Layers',
-              command: 'show-layers',
-              togglable: false,
-            },
-            {
-              id: 'show-styles',
-              active: true,
-              label: 'Style',
-              command: 'show-styles',
-              togglable: false,
-            }],
-          }
-        ]
-      },
-
-      selectorManager: {
-        appendTo: '.styles-container'
-      },
-
-      StyleManger: {
-        appendTo: '.styles-container',
-        sectors: [{
-          name: 'Dimension',
-          open: false,
-          buildProps: ['width', 'min-height', 'padding'],
-          properties: [
-            {
-              type: 'integer',
-              name: 'The width', 
-              property: 'width', 
-              units: ['px', '%'], 
-              defaults: 'auto',
-              min: 0, 
-            },
-            {
-              name: 'Extra',
-              open: false,
-              buildProps: ['background-color', 'box-shadow', 'custom-prop'],
-              properties: [
-                {
-                  id: 'custom-prop',
-                  name: 'Custom Label',
-                  property: 'font-size',
-                  type: 'select',
-                  defaults: '32px',
-                  options: [
-                    { value: '12px', name: 'Tiny' },
-                    { value: '18px', name: 'Medium' },
-                    { value: '32px', name: 'Big' },
-                  ],
-               }
-              ]
-            },
-          ]
-      }]
-    }
-
-})
-
-    editor.Panels.addPanel({
-        id: 'panel-top',
-        el: '.panel__top',
-      });
-      editor.Panels.addPanel({
-        id: 'basic-actions',
-        el: '.panel__basic-actions',
+  panels: {
+    defaults: [
+      {
+        id: "layers",
+        el: ".panel__right",
+        // Make the panel resizable
+        resizable: {
+          maxDim: 350,
+          minDim: 200,
+          tc: 0, // Top handler
+          cl: 1, // Left handler
+          cr: 0, // Right handler
+          bc: 0, // Bottom handler
+          keyWidth: "flex-basis",
+        },
+        id: "panel__switcher",
+        element: ".panel__switcher",
         buttons: [
           {
-            id: 'visibility',
-            active: true, // active by default
-            className: 'btn-toggle-borders',
-            label: '<u>B</u>',
-            command: 'sw-visibility', // Built-in command
-          }, {
-            id: 'export',
-            className: 'btn-open-export',
-            label: 'Exp',
-            command: 'export-template',
-            context: 'export-template', // For grouping context of buttons from the same panel
-          }, {
-            id: 'show-json',
-            className: 'btn-show-json',
-            label: 'JSON',
-            context: 'show-json',
-            command(editor) {
-              editor.Modal.setTitle('Components JSON')
-                .setContent(`<textarea style="width:100%; height: 250px;">
-                  ${JSON.stringify(editor.getComponents())}
-                </textarea>`)
-                .open();
-            },
-          }
+            id: "show-layers",
+            active: true,
+            label: "Layers",
+            command: "show-layers",
+            togglable: false,
+          },
+          {
+            id: "show-styles",
+            active: true,
+            label: "Style",
+            command: "show-styles",
+            togglable: false,
+          },
         ],
-      });
+      },
+    ],
+  },
 
-      editor.Commands.add('show-layers', {
-        getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
-        getLayersEl(row) { return row.querySelector('.layers-container') },
-      
-        run(editor, sender) {
-          const lmEl = this.getLayersEl(this.getRowEl(editor));
-          lmEl.style.display = '';
-        },
-        stop(editor, sender) {
-          const lmEl = this.getLayersEl(this.getRowEl(editor));
-          lmEl.style.display = 'none';
-        },
-      });
-      editor.Commands.add('show-styles', {
-        getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
-        getStyleEl(row) { return row.querySelector('.styles-container') },
-      
-        run(editor, sender) {
-          const smEl = this.getStyleEl(this.getRowEl(editor));
-          smEl.style.display = '';
-        },
-        stop(editor, sender) {
-          const smEl = this.getStyleEl(this.getRowEl(editor));
-          smEl.style.display = 'none';
-        },
-      });
+  selectorManager: {
+    appendTo: ".styles-container",
+  },
+
+  StyleManger: {
+    appendTo: ".styles-container",
+    sectors: [
+      {
+        name: "Dimension",
+        open: false,
+        buildProps: ["width", "min-height", "padding"],
+        properties: [
+          {
+            type: "integer",
+            name: "The width",
+            property: "width",
+            units: ["px", "%"],
+            defaults: "auto",
+            min: 0,
+          },
+          {
+            name: "Extra",
+            open: false,
+            buildProps: ["background-color", "box-shadow", "custom-prop"],
+            properties: [
+              {
+                id: "custom-prop",
+                name: "Custom Label",
+                property: "font-size",
+                type: "select",
+                defaults: "32px",
+                options: [
+                  { value: "12px", name: "Tiny" },
+                  { value: "18px", name: "Medium" },
+                  { value: "32px", name: "Big" },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
+
+editor.Panels.addPanel({
+  id: "panel-top",
+  el: ".panel__top",
+});
+editor.Panels.addPanel({
+  id: "basic-actions",
+  el: ".panel__basic-actions",
+  buttons: [
+    {
+      id: "visibility",
+      active: true, // active by default
+      className: "btn-toggle-borders",
+      label: "<u>B</u>",
+      command: "sw-visibility", // Built-in command
+    },
+    {
+      id: "export",
+      className: "btn-open-export",
+      label: "Exp",
+      command: "export-template",
+      context: "export-template", // For grouping context of buttons from the same panel
+    },
+    {
+      id: "show-json",
+      className: "btn-show-json",
+      label: "JSON",
+      context: "show-json",
+      command(editor) {
+        editor.Modal.setTitle("Components JSON")
+          .setContent(
+            `<textarea style="width:100%; height: 250px;">
+                  ${JSON.stringify(editor.getComponents())}
+                </textarea>`
+          )
+          .open();
+      },
+    },
+  ],
+});
+
+editor.Commands.add("show-layers", {
+  getRowEl(editor) {
+    return editor.getContainer().closest(".editor-row");
+  },
+  getLayersEl(row) {
+    return row.querySelector(".layers-container");
+  },
+
+  run(editor, sender) {
+    const lmEl = this.getLayersEl(this.getRowEl(editor));
+    lmEl.style.display = "";
+  },
+  stop(editor, sender) {
+    const lmEl = this.getLayersEl(this.getRowEl(editor));
+    lmEl.style.display = "none";
+  },
+});
+editor.Commands.add("show-styles", {
+  getRowEl(editor) {
+    return editor.getContainer().closest(".editor-row");
+  },
+  getStyleEl(row) {
+    return row.querySelector(".styles-container");
+  },
+
+  run(editor, sender) {
+    const smEl = this.getStyleEl(this.getRowEl(editor));
+    smEl.style.display = "";
+  },
+  stop(editor, sender) {
+    const smEl = this.getStyleEl(this.getRowEl(editor));
+    smEl.style.display = "none";
+  },
+});
